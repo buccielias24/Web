833cb12df198bf5b48a14d59b5fa2f7e4480bea4
elias <elias@LAPTOP-5BO7J5RO>
2020-01-29 11:31:42 -0300

    se quito el astrobus, se agrego condicion para planetas

--- src/servlet/CargaViaje.java
+++ src/servlet/CargaViaje.java
@@ -1,9 +1,7 @@
 package servlet;
 
 import java.io.IOException;
-import java.text.ParseException;
 import java.text.SimpleDateFormat;
-import java.util.Calendar;
 import java.util.Date;
 import java.util.concurrent.TimeUnit;
 
@@ -18,9 +16,8 @@ import entidades.Planeta;
 import entidades.Viaje;
 import logic.PlanetaControler;
 import logic.ViajeController;
-import entidades.Astrobus;
 import entidades.Ciudadano;
-import logic.AstrobusController;
+
 /**
  * Servlet implementation class CargaViaje
  */
@@ -42,12 +39,11 @@ public class CargaViaje extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		HttpSession session=request.getSession(false);
-		
+		//Se quito el astrobus de la carga
 		int origen=Integer.parseInt(request.getParameter("origen"));
-		int destino=Integer.parseInt(request.getParameter("destino"));
+	   	int destino=Integer.parseInt(request.getParameter("destino"));
 		String horaS=request.getParameter("horaSalida");
-		int astro= Integer.parseInt(request.getParameter("elegirAstrobus"));
-	    boolean frecuencia[]=new boolean[7];
+		boolean frecuencia[]=new boolean[7];
 	    frecuencia[0]=Boolean.parseBoolean(request.getParameter("Lunes"));
 	    frecuencia[1]=Boolean.parseBoolean(request.getParameter("Martes"));
 	    frecuencia[2]=Boolean.parseBoolean(request.getParameter("Miercoles"));
@@ -59,41 +55,49 @@ public class CargaViaje extends HttpServlet {
 	    String hasta=request.getParameter("hasta");
 	    
 	    
-	    Astrobus a=new Astrobus();
-		Planeta p1=new Planeta();
+	    Planeta p1=new Planeta();
 		Planeta p2=new Planeta();
 		
-		AstrobusController ac=new AstrobusController();
 		PlanetaControler pc=new PlanetaControler();
 		ViajeController vc=new ViajeController();
 		
 		p1.setIdPlaneta(origen);
 		p2.setIdPlaneta(destino);
-		a.setIdNave(astro);
 		
 		p1=pc.getById(p1);
 		p2=pc.getById(p2);
-		a=ac.getById(a);
-		
-		Viaje v=new Viaje();
-		v.setOrigen(p1);
-		v.setDestino(p2);
-        v.setAstrobus(a);		
-        v.setFrecuencia(frecuencia);
-        v.setDistancia(vc.getDistancia(v));
+			
+		  //Verificando estado del planeta para continuar con el proceso de carga
+			if(p1.getEstado()==false )
+				{
+					System.out.println("planeta de origen no disponible");
+					
+				}
+			else {	
+					if(p2.getEstado()==false)
+						{
+							System.out.println("planeta destino no disponible");
+						}
+							else {
+								Viaje v=new Viaje();
+								v.setOrigen(p1);
+								v.setDestino(p2);
+								v.setFrecuencia(frecuencia);
+								v.setDistancia(vc.getDistancia(v));
 
-        SimpleDateFormat formatter=new SimpleDateFormat("yyyy-MM-dd HH:mm");
-		Date[] fechas=vc.determinarFecha(desde, hasta, frecuencia, horaS);
-		for(int i=0; i<fechas.length ;i++)	
-		{		
-			v.setSalida(formatter.format(fechas[i]));
-			Date newDate=new Date(fechas[i].getTime()+TimeUnit.HOURS.toMillis(Math.round(v.getDistancia()/100)));
-			v.setLlegada(formatter.format(newDate));
-			System.out.println("salida: "+fechas[i]+" llegada : "+newDate);
-			vc.add(v,(Ciudadano)session.getAttribute("user"));
+								SimpleDateFormat formatter=new SimpleDateFormat("yyyy-MM-dd HH:mm");
+								Date[] fechas=vc.determinarFecha(desde, hasta, frecuencia, horaS);
+									for(int i=0; i<fechas.length ;i++)	
+										{		
+										v.setSalida(formatter.format(fechas[i]));
+										Date newDate=new Date(fechas[i].getTime()+TimeUnit.HOURS.toMillis(Math.round(v.getDistancia()/100)));
+										v.setLlegada(formatter.format(newDate));
+										System.out.println("salida: "+fechas[i]+" llegada : "+newDate);
+										vc.add(v,(Ciudadano)session.getAttribute("user"));
 		   
-		} 
-		
+										}
+					}
+			  }
 	}
 
 	/**
